e8536e2f3d5845de7973f5c183c47f51926eed05
reza-asad <rayza.asad@gmail.com>
2016-02-27 19:39:12 -0800

    first commit, adding the edges.txt

---
+++ Prim's_algorithm.py
@@ -0,0 +1,12 @@
+# Reza Asad
+# Algorithm Class
+# Feb 27th, 2016
+######################### Algorithms ########################
+# A non optimal greedy algorithm to find the schedueling time
+# for a set of jobs
+def prims_mst(G):
+    pass
+
+######################## Main ###############################
+# Extract the edges
+data_file = open('edges.txt')
