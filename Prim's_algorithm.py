48d4730d070fd50e874e4076fc0be9b80dc6ff1a
reza-asad <rayza.asad@gmail.com>
2016-02-28 15:10:38 -0800

    Naive implemenation using heaps which wastes space on a toy example

--- Prim's_algorithm.py
+++ Prim's_algorithm.py
@@ -2,11 +2,37 @@
 # Algorithm Class
 # Feb 27th, 2016
 ######################### Algorithms ########################
-# A non optimal greedy algorithm to find the schedueling time
-# for a set of jobs
+from heapq import heappush, heappop, heapify
+# This finds the minimum spanning tree of a graph using Prim's
+# greedy algorithm.
 def prims_mst(G):
-    pass
+    num_vertices = len(G)
+    seen = set()
+    # take a starting point.
+    # The alorithm does not depend on this.
+    source = G.iterkeys().next()
+    seen.add(source)
+    source_connection = list(G[source])
+    edge_costs =  source_connection
+    heapify(edge_costs)
 
+    selected_edges = []
+    total_cost = 0
+    while len(seen) != num_vertices:
+        # Add the next vertex
+        next_edge = heappop(edge_costs)
+        total_cost += next_edge[0]
+        selected_edges.append(next_edge[1])
+        added_vertex = next_edge[1][2]
+        seen.add(added_vertex)
+
+        # Maintain the edges_cost vertex addition
+        for edge in G[added_vertex]:
+            if edge[1][2] not in seen:
+                heappush(edge_costs, edge)
+    return selected_edges, total_cost
 ######################## Main ###############################
 # Extract the edges
-data_file = open('edges.txt')
+#data_file = open('edges.txt')
+graph = {'a':{(2, 'a-b'),(5,'a-c')}, 'b':{(1,'b-c'), (3,'b-d'), (2,'b-a')}, 'c':{(10,'c-d'),(1,'c-b'),(5,'c-a')}, 'd':{(10,'d-c'),(3,'d-b')}}
+print prims_mst(graph)
