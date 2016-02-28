760141f0a8da0145da84f39717ffbd0e4945fca9
Reza <rayza.asad@gmail.com>
2016-02-27 19:41:37 -0800

    Update Prim's_algorithm.py

--- Prim's_algorithm.py
+++ Prim's_algorithm.py
@@ -2,8 +2,6 @@
 # Algorithm Class
 # Feb 27th, 2016
 ######################### Algorithms ########################
-# A non optimal greedy algorithm to find the schedueling time
-# for a set of jobs
 def prims_mst(G):
     pass
 
